c2d5b9093e6bde8df21536f9b1c1c438c3920050
Deepthi <dpdevops6@gmail.com>
2024-03-05 19:41:59 -0500

    Terraform

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "main" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = var.ssh_sg_ingress_cidr
+    cidr_blocks = var.ssh_ingress_cidr
   }
 
   ingress {
--- userdata.sh
+++ userdata.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 yum install ansible -y &>>/opt/userdata.log
-
 ansible-pull -i localhost, -U https://github.com/dpathim/roboshop-ansible.git main.yml -e component=rabbitmq &>>/opt/userdata.log
