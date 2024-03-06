e5a40158870d322ada2b7aa331d8bab2adf9c411
Deepthi <dpdevops6@gmail.com>
2024-03-05 19:27:18 -0500

    Terraform

--- userdata.sh
+++ userdata.sh
@@ -2,4 +2,4 @@
 
 yum install ansible -y &>>/opt/userdata.log
 
-ansible-playbook -i localhost, -U https://github.com/dpathim/roboshop-ansible.git main.yml -e component=rabbitmq &>>/opt/userdata.log
+ansible-pull -i localhost, -U https://github.com/dpathim/roboshop-ansible.git main.yml -e component=rabbitmq &>>/opt/userdata.log
