6d6708844c54666bf123f7254cdea775433c18ac
Deepthi <dpdevops6@gmail.com>
2024-03-08 18:08:41 -0500

    Terraform

--- userdata.sh
+++ userdata.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 yum install ansible -y &>>/opt/userdata.log
+python3.11-pip.norch -y &>>/opt/userdata.log
+pip3.11 install botocore boto3 &>>/opt/userdata.log
+
 ansible-pull -i localhost, -U https://github.com/dpathim/roboshop-ansible.git main.yml -e component=rabbitmq &>>/opt/userdata.log
+
+
